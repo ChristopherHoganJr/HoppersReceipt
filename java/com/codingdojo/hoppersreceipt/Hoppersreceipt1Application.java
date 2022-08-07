6a667b2327075788b0678c84380e8b0246d882b8
Chris <55262451+ChristopherHoganJr@users.noreply.github.com>
2022-08-06 22:29:18 -0700

    Completed All Challenges

---
+++ java/com/codingdojo/hoppersreceipt/Hoppersreceipt1Application.java
@@ -0,0 +1,13 @@
+package com.codingdojo.hoppersreceipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hoppersreceipt1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hoppersreceipt1Application.class, args);
+	}
+
+}
